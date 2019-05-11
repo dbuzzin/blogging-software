d8cae2e04a6036c68c47daf0ffdab4a5ace90428
Danny Pearson <d.pearson1991@googlemail.com>
2019-05-11 18:35:42 +0100

    Test: share to facebook button

--- app.js
+++ app.js
@@ -65,3 +65,11 @@ app.listen(process.env.PORT || 80, (err) => {
     }
 });
 
+// app.listen(3000, (err) => {
+//     if(err) {
+//         console.log("Error: ", err);
+//     } else {
+//         console.log("Server running on port 3000");
+//     }
+// });
+
